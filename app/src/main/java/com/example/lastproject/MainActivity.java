abb93523ba2d8b4adfaf039b365660db34fd38e2
zahra seyfi <zahraa.tseyfi@gmail.com>
2021-06-11 14:44:37 +0430

    Ui Login & Sign Up

--- app/src/main/java/com/example/lastproject/MainActivity.java
+++ app/src/main/java/com/example/lastproject/MainActivity.java
@@ -2,13 +2,36 @@ package com.example.lastproject;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 public class MainActivity extends AppCompatActivity {
-
+    Button button1,button2;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        button1=findViewById(R.id.button);
+        button2=findViewById(R.id.button2);
+
+
+        button1.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                startActivity(new Intent(MainActivity.this,LoginActivity.class));
+            }
+        });
+
+        button2.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                startActivity(new Intent(MainActivity.this,SignActivity.class));
+            }
+        });
+
+
     }
 }
