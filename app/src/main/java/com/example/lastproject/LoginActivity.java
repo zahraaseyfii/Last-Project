737829a757c389440b76f0f6bc4a31449a006d67
zahra seyfi <zahraa.tseyfi@gmail.com>
2021-06-12 21:25:02 +0430

    This Part Completed !

--- app/src/main/java/com/example/lastproject/LoginActivity.java
+++ app/src/main/java/com/example/lastproject/LoginActivity.java
@@ -1,14 +1,75 @@
 package com.example.lastproject;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.app.ProgressDialog;
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.Toast;
+
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.Task;
+import com.google.firebase.auth.AuthResult;
+import com.google.firebase.auth.FirebaseAuth;
 
 public class LoginActivity extends AppCompatActivity {
+
+    Button button,button1;
+    EditText editText,editText1;
+
+    FirebaseAuth auth;
+
+    ProgressDialog  dialog;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_login);
+
+        dialog=new ProgressDialog(this);
+        dialog.setMessage("Please Wait . . . 🎶 ");
+
+        button=findViewById(R.id.button3);
+        button1=findViewById(R.id.button5);
+
+        editText=findViewById(R.id.editText);
+        editText1=findViewById(R.id.editText2);
+
+        auth= FirebaseAuth.getInstance();
+
+        button.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                dialog.show();
+                String email,password;
+                email=editText.getText().toString();
+                password=editText1.getText().toString();
+
+                auth.signInWithEmailAndPassword(email,password).addOnCompleteListener(new OnCompleteListener<AuthResult>() {
+                    @Override
+                    public void onComplete(@NonNull Task<AuthResult> task) {
+                        if(task.isSuccessful()){
+                            dialog.dismiss();;
+                            startActivity(new Intent(LoginActivity.this,MenuActivity.class));
+                        }
+                        else {
+                            Toast.makeText(LoginActivity.this,task.getException().getLocalizedMessage(),Toast.LENGTH_SHORT).show();;
+                        }
+                    }
+                });
+
+            }
+        });
+
+        button1.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                startActivity(new Intent(LoginActivity.this,SignActivity.class));
+            }
+        });
     }
 }
