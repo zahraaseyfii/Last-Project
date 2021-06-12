e7c28b4901df00b6aae7078d6a823e2f23c92609
zahra seyfi <zahraa.tseyfi@gmail.com>
2021-06-12 20:42:40 +0430

    Ui Login & Sign Up

--- app/src/main/java/com/example/lastproject/SignActivity.java
+++ app/src/main/java/com/example/lastproject/SignActivity.java
@@ -11,20 +11,24 @@ import android.widget.EditText;
 import android.widget.Toast;
 
 import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.OnSuccessListener;
 import com.google.android.gms.tasks.Task;
 import com.google.firebase.auth.AuthResult;
 import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.firestore.FirebaseFirestore;
 
 public class SignActivity extends AppCompatActivity {
     FirebaseAuth auth;
     EditText editText1,editText2;
     Button button1,button2;
+    FirebaseFirestore database;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_sign);
 
+        database=FirebaseFirestore.getInstance();
         auth=FirebaseAuth.getInstance();
         editText1=findViewById(R.id.editText4);
         editText2=findViewById(R.id.editText5);
@@ -45,10 +49,20 @@ public class SignActivity extends AppCompatActivity {
                 email=editText1.getText().toString();
                 pass=editText2.getText().toString();
 
+                final User user=new User();
+                user.setEmail(email);
+                user.setPass(pass);
+
                 auth.createUserWithEmailAndPassword(email,pass).addOnCompleteListener(new OnCompleteListener<AuthResult>() {
                     @Override
                     public void onComplete(@NonNull Task<AuthResult> task) {
                         if (task.isSuccessful()){
+                            database.collection("Users").document().set(user).addOnSuccessListener(new OnSuccessListener<Void>() {
+                                @Override
+                                public void onSuccess(Void aVoid) {
+                                    startActivity(new Intent(SignActivity.this,LoginActivity.class));
+                                }
+                            });
                             Toast.makeText(SignActivity.this,"Your Account is Created :)" ,Toast.LENGTH_SHORT ).show();
                         }
                         else{
---
+++ app/src/main/java/com/example/lastproject/User.java
@@ -0,0 +1,25 @@
+package com.example.lastproject;
+
+public class User {
+    private String email,pass;
+
+    public User(){
+
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public String getPass() {
+        return pass;
+    }
+
+    public void setPass(String pass) {
+        this.pass = pass;
+    }
+}
