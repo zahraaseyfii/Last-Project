57431c3b58df23bae03334c3baf32cf0e3c74e70
zahra seyfi <zahraa.tseyfi@gmail.com>
2021-06-14 17:18:59 +0430

    This Part Completed !

---
+++ app/src/main/java/com/example/lastproject/Cost.java
@@ -0,0 +1,50 @@
+package com.example.lastproject;
+
+public class Cost {
+    private String mablagh,daste,hesab,date,note;
+
+    public Cost()
+    {
+
+    }
+
+    public String getMablagh() {
+        return mablagh;
+    }
+
+    public void setMablagh(String mablagh) {
+        this.mablagh = mablagh;
+    }
+
+    public String getDaste() {
+        return daste;
+    }
+
+    public void setDaste(String daste) {
+        this.daste = daste;
+    }
+
+    public String getHesab() {
+        return hesab;
+    }
+
+    public void setHesab(String hesab) {
+        this.hesab = hesab;
+    }
+
+    public String getDate() {
+        return date;
+    }
+
+    public void setDate(String date) {
+        this.date = date;
+    }
+
+    public String getNote() {
+        return note;
+    }
+
+    public void setNote(String note) {
+        this.note = note;
+    }
+}
--- app/src/main/java/com/example/lastproject/Item1Fragment.java
+++ app/src/main/java/com/example/lastproject/Item1Fragment.java
@@ -1,7 +1,5 @@
 package com.example.lastproject;
 
-import android.content.Context;
-import android.net.Uri;
 import android.os.Bundle;
 
 import androidx.fragment.app.Fragment;
@@ -9,102 +7,79 @@ import androidx.fragment.app.Fragment;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.Toast;
+
+import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.firestore.FirebaseFirestore;
 
 
-/**
- * A simple {@link Fragment} subclass.
- * Activities that contain this fragment must implement the
- * {@link Item1Fragment.OnFragmentInteractionListener} interface
- * to handle interaction events.
- * Use the {@link Item1Fragment#newInstance} factory method to
- * create an instance of this fragment.
- */
 public class Item1Fragment extends Fragment {
-    // TODO: Rename parameter arguments, choose names that match
-    // the fragment initialization parameters, e.g. ARG_ITEM_NUMBER
-    private static final String ARG_PARAM1 = "param1";
-    private static final String ARG_PARAM2 = "param2";
 
-    // TODO: Rename and change types of parameters
-    private String mParam1;
-    private String mParam2;
-
-    private OnFragmentInteractionListener mListener;
 
     public Item1Fragment() {
-        // Required empty public constructor
     }
+    FirebaseFirestore database1;
+    Button button,button1;
+    EditText editText1,editText2,editText3,editText4,editText5;
 
-    /**
-     * Use this factory method to create a new instance of
-     * this fragment using the provided parameters.
-     *
-     * @param param1 Parameter 1.
-     * @param param2 Parameter 2.
-     * @return A new instance of fragment Item1Fragment.
-     */
-    // TODO: Rename and change types and number of parameters
-    public static Item1Fragment newInstance(String param1, String param2) {
-        Item1Fragment fragment = new Item1Fragment();
-        Bundle args = new Bundle();
-        args.putString(ARG_PARAM1, param1);
-        args.putString(ARG_PARAM2, param2);
-        fragment.setArguments(args);
-        return fragment;
-    }
+    DatabaseReference reference;
 
     @Override
     public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        if (getArguments() != null) {
-            mParam1 = getArguments().getString(ARG_PARAM1);
-            mParam2 = getArguments().getString(ARG_PARAM2);
-        }
+        database1=FirebaseFirestore.getInstance();
+        button= (button).findViewById(R.id.button7);
+        button1= (button1).findViewById(R.id.button8);
+
+
+        editText1=editText1.findViewById(R.id.editText3);
+        editText2=editText2.findViewById(R.id.editText6);
+        editText3=editText3.findViewById(R.id.editText7);
+        editText4=editText4.findViewById(R.id.editText9);
+        editText5=editText5.findViewById(R.id.editText11);
+
+        button.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                String mablagh,daste,hesab,date,note;
+                mablagh=editText1.getText().toString();
+                daste=editText2.getText().toString();
+                hesab=editText3.getText().toString();
+                date=editText4.getText().toString();
+                note=editText5.getText().toString();
+
+
+                //----------------------------------
+                final Cost cost=new Cost();
+                cost.setMablagh(mablagh);
+                cost.setDaste(daste);
+                cost.setHesab(hesab);
+                cost.setDate(date);
+                cost.setNote(note);
+                database1.collection("Costs");
+            }
+        });
+
+
+        button1.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+
+
+            }
+        });
     }
+
+
 
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
         // Inflate the layout for this fragment
         return inflater.inflate(R.layout.fragment_item1, container, false);
-    }
 
-    // TODO: Rename method, update argument and hook method into UI event
-    public void onButtonPressed(Uri uri) {
-        if (mListener != null) {
-            mListener.onFragmentInteraction(uri);
-        }
-    }
 
-    @Override
-    public void onAttach(Context context) {
-        super.onAttach(context);
-        if (context instanceof OnFragmentInteractionListener) {
-            mListener = (OnFragmentInteractionListener) context;
-        } else {
-            throw new RuntimeException(context.toString()
-                    + " must implement OnFragmentInteractionListener");
-        }
-    }
-
-    @Override
-    public void onDetach() {
-        super.onDetach();
-        mListener = null;
-    }
-
-    /**
-     * This interface must be implemented by activities that contain this
-     * fragment to allow an interaction in this fragment to be communicated
-     * to the activity and potentially other fragments contained in that
-     * activity.
-     * <p>
-     * See the Android Training lesson <a href=
-     * "http://developer.android.com/training/basics/fragments/communicating.html"
-     * >Communicating with Other Fragments</a> for more information.
-     */
-    public interface OnFragmentInteractionListener {
-        // TODO: Update argument type and name
-        void onFragmentInteraction(Uri uri);
     }
 }
